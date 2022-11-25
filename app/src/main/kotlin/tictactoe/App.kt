5f758ae184903128b41916f2178266661dc2a874
Craig Anderson <cranderson0110@gmail.com>
2022-11-25 15:43:17 +1300

    Don't overwrite existing positions, proper win checking, gameloop complete! Well, game complete!

--- app/src/main/kotlin/tictactoe/App.kt
+++ app/src/main/kotlin/tictactoe/App.kt
@@ -19,17 +19,34 @@ fun main() {
         print(board)
         print("${Board.CROSS.character} plays: ")
         try {
-            val input = readln().split(" ", ",", ", ")
-            val x = input[0].toInt()
-            val y = input[1].toInt()
-            board.placeCross(x, y)
+            var invalid = true
+            while (invalid) {
+                val input = readln().split(" ", ",", ", ")
+                val x = input[0].toInt()
+                val y = input[1].toInt()
+                invalid = !board.placeCross(x, y)
+            }
         } catch (e: Exception) {
             play = false
             continue
         }
-        val x = (0..2).random()
-        val y = (0..2).random()
-        board.placeCircle(x, y)
+        if (board.checkWin()) {
+            println("${Board.CROSS.character} wins!")
+            print(board)
+            play = false
+            continue
+        }
+        var invalid = true
+        while (invalid) {
+            val x = (0..2).random()
+            val y = (0..2).random()
+            invalid = !board.placeCircle(x, y)
+        }
         println()
+        if (board.checkWin()) {
+            println("${Board.CIRCLE.character} wins!")
+            print(board)
+            play = false
+        }
     }
 }
--- app/src/main/kotlin/tictactoe/Board.kt
+++ app/src/main/kotlin/tictactoe/Board.kt
@@ -27,22 +27,67 @@ class Board {
         board = Array (3) {Array(3) { EMPTY }}
     }
 
-    fun placeCircle(row: Int, column: Int) {
-        board[row][column] = CIRCLE
+    fun placeCircle(row: Int, column: Int): Boolean {
+        if (board[column][row] == EMPTY)
+            board[column][row] = CIRCLE
+        else
+            return false
+        return true
     }
 
-    fun placeCross(row: Int, column: Int) {
-        board[row][column] = CROSS
+    fun placeCross(row: Int, column: Int): Boolean {
+        if (board[column][row] == EMPTY)
+            board[column][row] = CROSS
+        else
+            return false
+        return true
+    }
+
+    fun checkWin(): Boolean {
+        return checkColumns() || checkRows() || checkDiagonals()
+    }
+
+    private fun checkColumns(): Boolean {
+        return checkColumn(0) || checkColumn(1) || checkColumn(2)
+    }
+
+    private fun checkColumn(index: Int): Boolean {
+        val column = listOf(board[index][0], board[index][1], board[index][2])
+        return column.all { it == CROSS } || column.all { it == CIRCLE }
+    }
+
+    private fun checkRows(): Boolean {
+        return checkRow(0) || checkRow(1) || checkRow(2)
+    }
+
+    private fun checkRow(index: Int): Boolean {
+        val row = listOf( board[2][index], board[0][index], board[1][index])
+        return row.all { it == CROSS } || row.all { it == CIRCLE }
+    }
+
+    private fun checkDiagonals(): Boolean {
+        return checkAscendingDiagonal() || checkDescendingDiagonal()
+    }
+
+    private fun checkAscendingDiagonal(): Boolean {
+        val diagonal = listOf(board[0][0], board[1][1], board[2][2])
+        return diagonal.all { it == CROSS } || diagonal.all { it == CIRCLE }
+    }
+
+    private fun checkDescendingDiagonal(): Boolean {
+        val diagonal = listOf(board[0][2], board[1][1], board[2][0])
+        return diagonal.all { it == CROSS } || diagonal.all { it == CIRCLE }
     }
 
     override fun toString(): String {
         var output = ""
+        output += "  0   1   2   \n"
         output += "-------------\n"
-        output += "| ${board[0][0].character} | ${board[0][1].character} | ${board[0][2].character} |\n"
+        output += "| ${board[0][0].character} | ${board[0][1].character} | ${board[0][2].character} |  0\n"
         output += "-------------\n"
-        output += "| ${board[1][0].character} | ${board[1][1].character} | ${board[1][2].character} |\n"
+        output += "| ${board[1][0].character} | ${board[1][1].character} | ${board[1][2].character} |  1\n"
         output += "-------------\n"
-        output += "| ${board[2][0].character} | ${board[2][1].character} | ${board[2][2].character} |\n"
+        output += "| ${board[2][0].character} | ${board[2][1].character} | ${board[2][2].character} |  2\n"
         output += "-------------\n"
         return output
     }
